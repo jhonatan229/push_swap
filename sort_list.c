d6e0ccad3c3761e92c9cc9d06b9d6501dd58dbfb
jhonatan229 <jhonatan229@outlook.com>
2021-09-13 21:36:54 -0300

    separated the sort by signal, first sorting the positive numbers, than sorting the negative numbers

--- sort_list.c
+++ sort_list.c
@@ -6,7 +6,7 @@
 /*   By: jestevam < jestevam@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 15:44:08 by jestevam          #+#    #+#             */
-/*   Updated: 2021/09/13 14:28:20 by jestevam         ###   ########.fr       */
+/*   Updated: 2021/09/13 21:34:31 by jestevam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,8 @@ static int	quantity_num(int num)
 	return (count);
 }
 
-static int max_places(int *lst, int size, int *max)
+//max começa com 11
+static int max_pos_places(int *lst, int size, int *max)
 {
 	int places;
 	int aux;
@@ -65,17 +66,39 @@ static int max_places(int *lst, int size, int *max)
 			places = aux;
 	}
 	*max = places;
+	count = -1;
 	places = 0;
-	count = 0;
-	while (count < size)
-	{
-		if (quantity_num(lst[count++]) == *max)
+	while (++count < size)
+		if (quantity_num(lst[count]) == *max && lst[count] >= 0)
 			places++;
-	}
 	return (places);
 }
 
-static int take_pos (int *lst, int size, int max)
+//max começa com 0
+static int max_neg_places(int *lst, int size, int *max)
+{
+	int places;
+	int aux;
+	int	count;
+
+	places = 10;
+	count = 0;
+	while (count < size)
+	{
+		aux = quantity_num(lst[count++]);
+		if (aux < places && aux > *max)
+			places = aux;
+	}
+	*max = places;
+	count = -1;
+	places = 0;
+	while (++count < size)
+		if (quantity_num(lst[count]) == *max && lst[count] < 0)
+			places++;
+	return (places);
+}
+
+static int take_positive_pos(int *lst, int size, int max)
 {
 	int num;
 	int pos;
@@ -86,7 +109,7 @@ static int take_pos (int *lst, int size, int max)
 	aux_size = size;
 	while (--size > -1)
 	{
-		if (quantity_num(lst[size]) == max)
+		if (quantity_num(lst[size]) == max && lst[size] >= 0)
 		{
 			if (lst[size] < num)
 			{
@@ -96,14 +119,40 @@ static int take_pos (int *lst, int size, int max)
 		}
 	}
 	if (pos == -1 && max <= 10)
-		pos = take_pos(lst, aux_size, max + 1);		
+		pos = take_positive_pos(lst, aux_size, max + 1);		
 	return (pos);
 }
+
+static int take_negative_pos(int *lst, int size, int max)
+{
+	int num;
+	int pos;
+	int aux_size;
+
+	num = 0;
+	pos = -1;
+	aux_size = size;
+	while (--size > -1)
+	{
+		if (quantity_num(lst[size]) == max && lst[size] < 0)
+		{
+			if (lst[size] < num)
+			{
+				num = lst[size];
+				pos = size;
+			}
+		}
+	}
+	if (pos == -1 && max < 0)
+		pos = take_negative_pos(lst, aux_size, max - 1);		
+	return (pos);
+}
+
 static void verify_pos_to_push(t_lists *lst, int pos)
 {
 	while (pos != lst->size_a - 1)
 	{
-		print_list(lst);
+		//print_list(lst);
 		if (pos <= lst->size_a / 2)
 		{
 			reverse_rotate_list(lst->list_a, lst->size_a);
@@ -124,34 +173,67 @@ static void push_number(t_lists *lst, int pos)
 {
 	verify_pos_to_push(lst, pos);
 	push_num_to_lst(lst, 0);
+	//print_list(lst);
+}
+
+static void sort_positive_numbers(t_lists *lst, int *max_place)
+{
+	int quantity;
+	int pos;
+
+	quantity = max_pos_places(lst->list_a, lst->size_a, max_place);
+	while (quantity != 0)
+	{
+		pos = take_positive_pos(lst->list_a, lst->size_a, *max_place);
+		printf("max %i, pos_min %i, quanti: %i\n", *max_place, pos, quantity);
+		print_list(lst);
+		push_number(lst, pos);
+		quantity--;
+	}
+	pos = take_positive_pos(lst->list_a, lst->size_a, *max_place + 1);
+	printf("pos: %i\n", pos);
+	if (pos > -1)
+		verify_pos_to_push(lst, pos);
+	while (lst->size_b)
+		push_num_to_lst(lst, 1);
+	print_list(lst);
+}
+
+static void sort_negative_numbers(t_lists *lst, int *max_place)
+{
+	int quantity;
+	int pos;
+
+	quantity = max_neg_places(lst->list_a, lst->size_a, max_place);
+	while (quantity != 0)
+	{
+		pos = take_negative_pos(lst->list_a, lst->size_a, *max_place);
+		printf("max %i, pos_min %i\n", *max_place, pos);
+		print_list(lst);
+		push_number(lst, pos);
+		quantity--;
+	}
+	pos = take_negative_pos(lst->list_a, lst->size_a, *max_place - 1);
+	if (pos > -1)
+		verify_pos_to_push(lst, pos);
+	while (lst->size_b)
+		push_num_to_lst(lst, 1);
 	print_list(lst);
 }
 
 int	sort_list(t_lists *lst)
 {
-	int max_place;
-	int pos;
-	int quantity;
+	int max_positive_place;
+	int max_negative_place;
 
-	max_place = 11;
-	quantity = max_places(lst->list_a, lst->size_a, &max_place);
+	max_positive_place = 11;
+	max_negative_place = 0;
 	while (verify_sort_list(lst->list_a, lst->size_a, 1) || lst->size_b)
 	{
-		while (quantity != 0)
-		{
-			pos = take_pos(lst->list_a, lst->size_a, max_place);
-			printf("max %i, pos_min %i\n", max_place, pos);
-			push_number(lst, pos);
-			quantity--;
-		}
-		pos = take_pos(lst->list_a, lst->size_a, max_place + 1);
-		printf("pos: %i\n", pos);
-		if (pos > -1)
-			verify_pos_to_push(lst, pos);
-		while (lst->size_b)
-			push_num_to_lst(lst, 1);
-		print_list(lst);
-		quantity = max_places(lst->list_a, lst->size_a, &max_place);
+		if (max_positive_place != 0)
+			sort_positive_numbers(lst, &max_positive_place);
+		else if (max_negative_place != 10)
+			sort_negative_numbers(lst, &max_negative_place);
 		//if (max_place == 0)
 		//	exit(1);
 	}
